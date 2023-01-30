296e7ec5d121ec7c5434e8c65d1f34614f7f7601
Alexander <akoceryzhkin@gmail.com>
2023-01-30 15:24:17 +0300

    first

---
+++ script.js
@@ -0,0 +1,46 @@
+'use strict';
+// let age = 29;
+// let user = "Alex"; 
+// let message = 'Hello';
+
+// console.log(age);
+// console.log(user);
+// console.log(message);
+
+// const COLOR_RED = "f00";
+// let color = COLOR_RED;
+// console.log(color);
+
+// alert("hello");
+// prompt("Hi");
+// let result = prompt('text');
+// let age = prompt('Сколько тебе лет?', 28);
+// alert(`Тебе ${age} лет!`)
+// let result = confirm ("Тебя зовут Андрей?");
+// alert(result);
+
+// console.log(2*3);
+// let i = 2;
+// i = -i;
+// console.log(i);
+
+// let x = 2;
+// let y = 3;
+// console.log(y-x);
+
+// console.log(5%2);
+// console.log(2**3);
+// console.log(4**1/2);
+// let text = 3 + 3 + "text";
+// console.log(text);
+
+// let x = -2;
+// console.log(+x);
+
+// let x = "1";
+// let y = "2";
+// console.log(x+y);   
+
+let x = "1";
+let y = "2";
+console.log(+x + +y);   
